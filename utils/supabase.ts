d5f1aada3817c8a738b5ae77e31d1f840d795a6b
ArthurOliveira120 <arthuromarinho10@gmail.com>
2025-07-22 12:15:55 +0000

    supabase client

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://pskvsqwbwfyisvqtvxob.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBza3ZzcXdid2Z5aXN2cXR2eG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExNTE1MjAsImV4cCI6MjA2NjcyNzUyMH0.3TFdRh4KIIwz_tSGrSjeJBvamosIv9sNyek8ZxfmA1Y";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
